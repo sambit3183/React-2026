a9f95b7313161a470d9ffde5bb94c49a5d0df17f
sambit3183 <sambitranjansahoo5@gmail.com>
2026-05-16 13:34:09 +0530

    4th uload

--- 03-react-component/src/App.jsx
+++ 03-react-component/src/App.jsx
@@ -2,13 +2,19 @@
 
 import React from 'react'
 import "./App.css";
+import Card from './components/Card';
 
 const App = () => {
   return (
     <div className="container">
-      <h1>Hello React Bruhh !!!</h1>
+      {/* <h1>Hello React Bruhh !!!</h1> */}
+      <Card/>
+      <Card/>
+      <Card/>
       </div>
   )
 }
 
-export default App
+export default App;
+
+//xml
---
+++ 03-react-component/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className = 'card-container'>
+
+            <img src="https://media.licdn.com/dms/image/v2/D5603AQGJH793FmbkuQ/profile-displayphoto-scale_100_100/B56Z4ngLLGIEAc-/0/1778779206986?e=1780531200&v=beta&t=-YISQEvaq1cIwUlwzesBxb0QefT8La-nxa3fqERJZVY" alt="profile_pic" />
+            <h2>Name :Sambit Ranjan Sahoo  </h2>
+            <p>DOB :17.12.2006 </p>
+            <button>Follow</button>
+
+    </div>
+  )
+}
+
+export default Card
